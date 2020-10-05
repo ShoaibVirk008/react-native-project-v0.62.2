7b33559f299d188a7af5e76ec6d286c2b228c1f2
shoaibvirk008 <shoaibvirk008@gmail.com>
2020-10-05 15:15:14 +0500

    restructured

---
+++ src/components/index.js
@@ -0,0 +1,74 @@
+
+export {
+    ButtonColored,
+    ButtonColoredSmall,
+    ButtonBordered,
+    ButtonBorderedSmall
+} from './buttons';
+
+export {
+    XXLTitle,
+    XLTitle,
+    LargeTitle,
+    MediumTitle,
+    SmallTitle,
+    TinyTitle,
+    LargeText,
+    MediumText,
+    RegularText,
+    SmallText,
+    TinyText,
+    DrawerLable
+} from './text';
+
+export {
+    BackIcon,
+    CustomIcon,
+    IconWithText,
+    MenuIcon,
+    FilterIcon,
+    IconButton,
+    TouchableCustomIcon
+} from './icons'
+
+export {
+    MainWrapper,
+    MainWrapperPrimary,
+    ImageBackgroundWrapper,
+    MaterialBackgroundWrapper,
+    Wrapper,
+    RowWrapper,
+    ComponentWrapper,
+    RowWrapperBasic,
+    AbsoluteWrapper,
+    HeaderWrapperPrimary,
+    FooterWrapperPrimary,
+    CardWrapper,
+} from './wrappers'
+export {
+    Spacer
+} from './spacers'
+
+export {
+    TextInputColored,
+    TextInputBordered
+} from './textInput'
+
+export {
+    LogoMain
+} from './logos'
+
+export {
+    Card
+} from './Cards'
+
+export {
+    LineHorizontal
+} from './lines'
+
+export {
+    ImageRound,
+    ImageSqareRound,
+    ImageCollectionItem,
+    ImageBackgroundTop
+} from './images'
--- src/themes/index.js
+++
@@ -1,9 +0,0 @@
-import colors from './colors'
-import fontFamily from './fontFamily'
-import fontSize from './fontSize'
-import metrics from './metrics'
-import images from './images'
-import appStyles from './appStyles'
-import icons from './icons'
-
-export { colors, fontFamily, fontSize, images, metrics, appStyles, icons }
