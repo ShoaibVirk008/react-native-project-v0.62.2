4d2780d18b1e53afd6d21db9a6af90845969d43e
shoaibvirk008 <shoaibvirk008@gmail.com>
2020-10-08 15:37:19 +0500

    scroll view added

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,16 @@
 
 export * from './buttons';
-
 export * from './text';
-
 export * from './icons'
-
 export * from './wrappers'
 export *from './spacers'
-
 export * from './textInput'
-
 export * from './logos'
-
 export * from './Cards'
-
 export * from './lines'
-
 export * from './images'
 export * from './chatBubbles'
 export * from './checkBoxs'
+export * from './scrollViews'
 
 
